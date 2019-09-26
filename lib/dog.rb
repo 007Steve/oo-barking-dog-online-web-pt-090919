5b901f358e63794c3d72934ea32601fe155d61a7
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-26 04:45:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,15 +6,16 @@ class Dog
     puts "woof!"
   end
 end
-class Dog 
-  def name=(setter)
-    this_dog_name = setter
-  end
-  def name=(getter)
-    this_dog_name = getter
-end
+#class Dog 
+  #def name=(setter)
+#    this_dog_name = setter
+ # end
+ # def name=(getter)
+  #  this_dog_name = getter
+#end
 
-def bark 
-  puts "woof!"
-end
-end
+#def bark 
+#  puts "woof!"
+#end
+#end
+# not sure why this worked
