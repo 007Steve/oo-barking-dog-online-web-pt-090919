60d0ae938696e7ca735a49bcebcd56ad20b8eeb9
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-26 04:40:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,1 @@
 # Your code goes here!
-class Dog 
-  def name=(setter)
-    this_dog_name = setter
-  end
-  def name=(getter)
-    this_dog_name = getter
-end
-
-def bark 
-  puts "woof!"
-end
-end
